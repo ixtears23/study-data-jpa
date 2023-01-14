aa9c627f6c692ca86b16295ce3516621f6022601
ixtears23 <ixtears23@icloud.com>
2023-01-15 02:04:50 +0900

    Team 도메인 모델 추가 및 Lazy Loading 적용

--- src/main/java/junseok/snr/studydatajpa/entity/Member.java
+++ src/main/java/junseok/snr/studydatajpa/entity/Member.java
@@ -1,25 +1,39 @@
 package junseok.snr.studydatajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"})
 public class Member {
 
-    protected Member() {
-    }
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String userName;
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String userName) {
         this.userName = userName;
     }
 
-    @Id @GeneratedValue
-    private Long id;
-    private String userName;
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }
