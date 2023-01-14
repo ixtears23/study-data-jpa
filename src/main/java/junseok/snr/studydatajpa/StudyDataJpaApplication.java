c7252dd0d407ba8c94bf1638de5e7858db0a9a13
junseokOh <ixtears23@icloud.com>
2023-01-14 17:32:57 +0900

    프로젝트 생성

---
+++ src/main/java/junseok/snr/studydatajpa/StudyDataJpaApplication.java
@@ -0,0 +1,13 @@
+package junseok.snr.studydatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/junseok/snr/studydatajpa/StudyDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.studydatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
