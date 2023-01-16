f384c814e9b5d68028ac86b99cd5900889699a7e
ixtears23 <ixtears23@icloud.com>
2023-01-17 07:27:08 +0900

    Web 에서 paging 처리

--- src/main/java/junseok/snr/studydatajpa/controller/MemberController.java
+++ src/main/java/junseok/snr/studydatajpa/controller/MemberController.java
@@ -1,8 +1,15 @@
 package junseok.snr.studydatajpa.controller;
 
+import junseok.snr.studydatajpa.dto.MemberDto;
 import junseok.snr.studydatajpa.entity.Member;
+import junseok.snr.studydatajpa.entity.Team;
 import junseok.snr.studydatajpa.repository.MemberRepository;
+import junseok.snr.studydatajpa.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +21,7 @@ import javax.annotation.PostConstruct;
 public class MemberController {
 
     private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
 
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Long id) {
@@ -27,9 +35,18 @@ public class MemberController {
         return member.getUserName();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "userName") Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            final Team team = new Team("team" + i);
+            teamRepository.save(team);
+            memberRepository.save(new Member("user" + i, i, team));
+        }
     }
 }
--- src/main/java/junseok/snr/studydatajpa/dto/MemberDto.java
+++ src/main/java/junseok/snr/studydatajpa/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package junseok.snr.studydatajpa.dto;
 
+import junseok.snr.studydatajpa.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,4 +11,10 @@ public class MemberDto {
     private Long id;
     private String userName;
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.userName = member.getUserName();
+        this.teamName = member.getTeam().getName();
+    }
 }
